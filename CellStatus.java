7ebd5bcabf62f2dcf952cf8fb709b4251ad574ee
vivialvianita <vivialvianita0109@gmail.com>
2024-12-04 17:43:23 +0700

    CellStatus

--- CellStatus.java
+++ CellStatus.java
@@ -1,5 +1,8 @@
-package Sudoku;
-
+package sudoku;
+/**
+ * An enumeration of constants to represent the status
+ * of each cell.
+ */
 public enum CellStatus {
     GIVEN,         // clue, no need to guess
     TO_GUESS,      // need to guess - not attempted yet
