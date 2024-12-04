48c779732204a02c8289f5a53761a3f78649c38b
vivialvianita <vivialvianita0109@gmail.com>
2024-12-04 17:42:51 +0700

    Puzzle

---
+++ Puzzle.java
@@ -0,0 +1,56 @@
+package sudoku;
+
+import sudoku.SudokuConstants;
+
+public class Puzzle {
+    // All variables have package access
+    // The numbers on the puzzle
+    int[][] numbers = new int[SudokuConstants.GRID_SIZE][SudokuConstants.GRID_SIZE];
+    // The clues - isGiven (no need to guess) or need to guess
+    boolean[][] isGiven = new boolean[SudokuConstants.GRID_SIZE][SudokuConstants.GRID_SIZE];
+
+    // Constructor
+    public Puzzle() {
+        super();
+    }
+
+    // Generate a new puzzle given the number of cells to be guessed, which can be used
+    //  to control the difficulty level.
+    // This method shall set (or update) the arrays numbers and isGiven
+    public void newPuzzle(int cellsToGuess) {
+        // Hardcoded puzzle (solved Sudoku grid)
+        int[][] hardcodedNumbers =
+                {{5, 3, 4, 6, 7, 8, 9, 1, 2},
+                        {6, 7, 2, 1, 9, 5, 3, 4, 8},
+                        {1, 9, 8, 3, 4, 2, 5, 6, 7},
+                        {8, 5, 9, 7, 6, 1, 4, 2, 3},
+                        {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                        {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                        {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                        {2, 8, 7, 4, 1, 9, 6, 3, 5},
+                        {3, 4, 5, 2, 8, 6, 1, 7, 9}};
+
+        // Copy the hardcoded numbers into the "numbers" array
+        for (int row = 0; row < SudokuConstants.GRID_SIZE; ++row) {
+            for (int col = 0; col < SudokuConstants.GRID_SIZE; ++col) {
+                numbers[row][col] = hardcodedNumbers[row][col];
+                isGiven[row][col] = true; // Mark all as given initially
+            }
+        }
+
+        // Randomly remove cells to create a puzzle with the desired number of cells to guess
+        int removed = 0;
+        while (removed < cellsToGuess) {
+            int row = (int) (Math.random() * SudokuConstants.GRID_SIZE);
+            int col = (int) (Math.random() * SudokuConstants.GRID_SIZE);
+
+            if (isGiven[row][col]) {
+                isGiven[row][col] = false; // Mark cell as not given
+                removed++;
+            }
+        }
+    }
+
+
+    //(For advanced students) use singleton design pattern for this class
+}
