cec454c33df95a32fe6ec9326284574b33be0125
Jannati Urfa Muhayat <162418616+urfayyaa@users.noreply.github.com>
2024-12-06 22:30:28 +0700

    Nama

--- SudokuMain.java
+++ SudokuMain.java
@@ -1,3 +1,12 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026221161 - Ryan Adi Putra Pratama
+ * 2 - 5026231185 - Jannati Urfa Muhayat
+ * 3 - 5026231226 - Vivi Alvianita
+ */
 package sudoku;
 
 import sudoku.GameBoardPanel;
